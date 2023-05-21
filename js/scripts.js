e476ebccdc526ef3f98dedd90ed9d674158283c8
Fuad Hasan <fuad32.ice.ru@gmail.com>
2023-05-21 18:52:45 +0600

    Initial agency Web app Not responsive yet

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+const ans1 = document.getElementById('faq-ans1');
+// const icon2 = document.getElementById('faq-icon2');
+const q1 = document.getElementById('faq-q1');
+
+
+let ansMe1 = event =>{
+    ans1.style.display="block";
+    q1.style.display="none";
+}
+
+let qMe1 = event =>{
+    ans1.style.display="none";
+    q1.style.display="block";
+}
+
+const ans2 = document.getElementById('faq-ans2');
+// const icon2 = document.getElementById('faq-icon2');
+const q2 = document.getElementById('faq-q2');
+
+
+let ansMe2 = event =>{
+    ans2.style.display="block";
+    q2.style.display="none";
+}
+
+let qMe2 = event =>{
+    ans2.style.display="none";
+    q2.style.display="block";
+}
+
+const ans3 = document.getElementById('faq-ans3');
+// const icon2 = document.getElementById('faq-icon2');
+const q3 = document.getElementById('faq-q3');
+
+
+let ansMe3 = event =>{
+    ans3.style.display="block";
+    q3.style.display="none";
+}
+
+let qMe3 = event =>{
+    ans3.style.display="none";
+    q3.style.display="block";
+}
+
+
+const ans4 = document.getElementById('faq-ans4');
+// const icon2 = document.getElementById('faq-icon2');
+const q4 = document.getElementById('faq-q4');
+
+
+let ansMe4 = event =>{
+    ans4.style.display="block";
+    q4.style.display="none";
+}
+
+let qMe4 = event =>{
+    ans4.style.display="none";
+    q4.style.display="block";
+}
